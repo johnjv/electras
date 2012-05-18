d18459aaf1178ca555d5d1e634dc12d444a9dc8c
Thierry Kimenyi <Thierry Kimenyi@localhost>
2012-05-18 09:28:33 -0500

    Floor new updates

---
+++ Floor/FactoryFloor.js
@@ -0,0 +1,268 @@
+$(document).ready(function(){
+	//"use strict";
+	var curid = 0;
+	
+	
+	
+	$("#request").click(function(){
+		var value, picture;
+		value = $("#requestArea").val();
+		if(value == "") {
+				console.log("you requested nothing man");
+		}
+		else{
+			console.log(value);
+			$("#requestedCandy").text(value);
+			picture = setCandyPicture(value);
+			$("requestedCandy").prepend('<img src="candy22.png"></img>');
+		}
+		
+	});
+		
+	$("#start").click(function(){
+		startMachine();
+	});
+	
+	
+	
+	
+	function startMachine(){
+		var items = createSequence();
+		var count =0;
+		
+		function startNext(){
+			if(count < 8) {
+				placeCandy(items[count]);
+				moveCandy(items[count]);
+				count++;
+				setTimeout(startNext, 3000);
+			}
+			/*
+			if(count === 0){
+				for(var i = 0; i < 8; i += 1){
+					finalItems.push(items[i]);
+				}
+			}*/
+		}
+		startNext();
+	}
+	
+	$("#advanceLevel").click(function(){
+		advanceSelector(); // method created by the Levels/Judge team
+	});
+	
+	$("#viewLevels").click(function(){
+		showSelector(); // method created by the Levels/Judge team
+	});
+
+	function Candy(type){
+		this.type = type;
+		this.picture = "";
+	}
+	
+	Candy.prototype.getCandyType = function(){
+		return this.type;
+	};
+	
+	Candy.prototype.setCandyPicture = function(){	
+		var candy;
+		candy = this.type;
+		candy = $.trim(candy);
+		
+		i = $.inArray('CRGY', candy.substring(0, 1)); // check that to see if there is any progress
+		
+		if (candy == 'Co') {
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy00.png"></img>');
+		}
+		
+		else if (candy == 'C|'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy01.png"></img>');
+		}
+		
+		else if (candy == 'C-'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy02.png"></img>');
+		}
+		
+		else if (candy == 'Ro'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy10.png"></img>');
+		}
+		
+		else if (candy == 'R|'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy11.png"></img>');
+		}
+		
+		else if (candy == 'R-'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy12.png"></img>');
+		}
+		
+		else if (candy == 'Go'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy20.png"></img>');
+		}
+		
+		else if (candy == 'G|'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy21.png"></img>');
+		}
+		
+		else if (candy == 'G-'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy22.png"></img>');
+		}
+		
+		else if (candy == 'Yo'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy30.png"></img>');
+		}
+		
+		else if (candy == 'Y|'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy31.png"></img>');
+		}
+		
+		else if (candy == 'Y-'){
+			this.picture = $('<img src="file:///export/home/s12/kimenyiti/Desktop/Odyssey/Electra/resource-image/candy32.png"></img>');
+		}
+		
+		else{
+			$("#requestedCandy").text("Your candy is not available");
+		}
+		return this.picture;
+	};
+	
+	function setCurId(levelid){
+		curid = levelid;
+		console.log("set curid to: " + curid );
+	}
+	
+	function getLevelType(){
+		var level = getCurrentLevel(curid);
+		var typeInfo = new Array();
+		var circuitInfo = new Array();
+		var levelInfo = new Array();
+		
+		for (var i=0; i< level.analysis.length; i+=1){
+		 	typeInfo[i] = new Candy(level.analysis[i].type);
+		}
+		
+		for (var i=0; i< level.analysis.length; i+=1){
+		 	circuitInfo[i] = new Candy(level.analysis[i].circuitSays);
+		}
+		
+		for (var i=0; i< level.analysis.length; i+=1){
+		 	levelInfo[i] = new Candy(level.analysis[i].levelSays);
+		}
+		 
+		 return typeInfo;
+	}
+	
+	function createSequence(){
+		var picArray = [];
+		var list = getLevelType();
+		var j;
+		console.log(list);
+		for (j = 0; j<list.length;j+=1){			
+			picArray.push(list[j].setCandyPicture());
+		}
+		return picArray;
+	}
+	
+	function placeCandy(candy){
+		"user strict";
+		var machine, candyPos, items;
+        candy.width(20);
+        machine = $('#machine');
+        machine.append(candy);
+        $(candy).each(function(){
+        	candyPos = candy.offset();
+        	candyPos.left += machine.width();  // 450 should be machine.width()
+        	candyPos.top += machine.height()/2 - 60; // should be machine.height() minize something
+        	//console.log("candy top: " + candyPos.top);
+        	//console.log("candy left: " + candyPos.left);
+        	//console.log(machine.width());
+       	 	candy.offset(candyPos);
+        	machine.append(candy);
+        	console.log("just added");
+        });
+        
+        return candy;
+        
+    }
+    
+    
+    function moveCandy(candy){
+    	"user strict";
+    	var machine;
+    	machine = $('#machine');
+    	$(candy).animate({
+        	left: '-=400'
+        }, 3000); 
+        $(candy).animate({
+        	left: '-=50',
+        	width: '-=10',
+        	
+        }, 500); 
+    }
+	
+	
+	function levelChanged(oldLevel, newLevel){
+		"user strict";
+		//TODO
+		level = newLevel;
+		updateChalkBoard();
+	}
+	
+	function updateChalkBoard(){
+		"user strict"
+		var orderText;
+		orderText = Level.orderText; // from the Level team
+		$('#chalkBoard').text(orderText);
+	}
+
+	function getleverLocation(){
+		"user strict";
+		var leverLocation, leverPos;
+		leverPos = $("#lever").offset();
+		leverLocation = new Object();
+		leverLocation.x = leverPos.left;
+		leverLocation.y = leverPos.top;
+		leverLocation.width = $("#lever").width();
+		leverlocation.height = $("#lever").height();
+		
+		return leverLocation;
+		
+	}
+
+	function getAdvanceLocation(){
+		"user strict";
+		var advanceLocation, advancePos;
+		advancePos = $("#advanceLevel").offset();
+		advanceLocation = new Object();
+		advanceLocation.x = advancePos.left;
+		advanceLocation.y = advancePos.top;
+		advanceLocation.width = $("#advanceLevel").width();
+		advanceLocation.height = $("#advanceLevel").height();
+		
+		return advanceLocation;
+	}
+	
+	function howToDisplay() {
+		"user strict";
+		//TODO 	This should indicate whether the candy goes in the box or in the hole, it will call putInBox
+		var listToDisplay = new Array();
+		var isAccepted = new Boolean();
+		
+		//for ()
+		
+		//if (SequenceItem.circuitSays) {
+			isAccepted = true;
+		//}
+	}
+	
+	function putInBox(candy){
+		"user strict";
+		//TODO this will change the css properties of the element that is to go in the box i.e being accepted
+		//var top, newPos
+		//ne
+	}
+	
+	setCurId(1);
+	
+	
+
+});
---
+++ workshop/drawcirc.js
@@ -0,0 +1,212 @@
+(function (my, $) {
+	"use strict";
+	var WIRE_WIDTH = 3;
+	var CONNECT_RADIUS = 3;
+	
+	function setterImage(elt) {
+		return function (imgName) {
+			var newName = 'resource/' + imgName + '.png';
+			if (elt.imgElt.attr('src') !== newName) {
+				elt.imgElt.attr('src', newName);
+			}
+		};
+	}
+
+	my.DrawCirc = {};
+
+	my.DrawCirc.createElement = function (info, elt) {
+		var type, offs0, img;
+
+		if (elt.imgElt) {
+			elt.imgElt.remove();
+		}
+
+		type = elt.type;
+		offs0 = info.canvas.offset();
+		img = $('<img></img>').addClass('on_canvas');
+		elt.imgElt = img;
+		elt.setImage = setterImage(elt);
+		elt.setImage(type.imgName);
+		img.width(type.imgWidth);
+		img.height(type.imgHeight);
+		img.offset({
+			left: offs0.left + elt.x + elt.type.imgX,
+			top: offs0.top + elt.y + elt.type.imgY
+		});
+		info.canvas.append(img);
+
+		$.each(elt.conns, function (i, conn) {
+			my.DrawCirc.createStub(info, conn);
+		});
+	};
+
+	my.DrawCirc.removeElement = function (info, elt) {
+		$.each(elt.conns, function (i, conn) {
+			if (conn.circ !== null) {
+				conn.circ.remove();
+			}
+			if (conn.stub !== null) {
+				conn.stub.remove();
+			}
+		});
+		elt.imgElt.remove();
+	};
+
+	my.DrawCirc.repositionElement = function (info, elt) {
+		var type, offs0, img;
+
+		type = elt.type;
+		offs0 = info.canvas.offset();
+		elt.imgElt.offset({
+			left: offs0.left + elt.x + elt.type.imgX,
+			top: offs0.top + elt.y + elt.type.imgY
+		});
+
+		$.each(elt.conns, function (i, conn) {
+			my.DrawCirc.createStub(info, conn);
+		});
+	};
+
+	my.DrawCirc.createStub = function (info, conn) {
+		var cx, cy, dx, dy;
+
+		if (conn.circ !== null) {
+			conn.circ.remove();
+		}
+		if (conn.stub !== null) {
+			conn.stub.remove();
+		}
+
+		cx = conn.elt.x + conn.x;
+		cy = conn.elt.y + conn.y;
+		dx = cx + conn.dx;
+		dy = cy + conn.dy;
+		if (conn.input) {
+			conn.circ = info.paper.circle(cx, cy, CONNECT_RADIUS);
+		} else {
+			conn.circ = info.paper.circle(cx, cy, CONNECT_RADIUS);
+			conn.circ.attr('fill', 'black');
+		}
+		if (conn.circ !== null) {
+			if (conn.dx > 0) {
+				cx += CONNECT_RADIUS;
+			} else if (conn.dx < 0) {
+				cx -= CONNECT_RADIUS;
+			}
+			if (conn.dy > 0) {
+				cy += CONNECT_RADIUS;
+			} else if (conn.dy < 0) {
+				cy -= CONNECT_RADIUS;
+			}
+		}
+		conn.stub = info.paper.path('M' + cx + ',' + cy
+			+ 'L' + dx + ',' + dy);
+		conn.stub.attr('stroke-width', WIRE_WIDTH);
+		if (conn.conns.length > 0) {
+			conn.stub.hide();
+			if (conn.input) {
+				conn.circ.hide();
+			}
+		}
+		my.DrawCirc.recolorConnection(info, conn);
+	};
+
+	my.DrawCirc.hideStub = function (info, conn) {
+		if (conn) {
+			conn.stub.hide();
+			if (conn.circ) {
+				conn.circ.hide();
+			}
+		}
+	};
+
+	my.DrawCirc.showStub = function (info, conn) {
+		if (conn) {
+			conn.stub.show();
+			if (conn.circ) {
+				conn.circ.show();
+			}
+		}
+	};
+
+	my.DrawCirc.createWire = function (info, conn0, conn1) {
+		var c0, c1, x0, y0, x1, y1, line;
+		if (conn0.input) {
+			c0 = conn1;
+			c1 = conn0;
+		} else {
+			c0 = conn0;
+			c1 = conn1;
+		}
+		x0 = c0.elt.x + c0.x;
+		y0 = c0.elt.y + c0.y;
+		x1 = c1.elt.x + c1.x;
+		y1 = c1.elt.y + c1.y;
+		line = info.paper.path('M' + (x0 + c0.dx) + ',' + (y0 + c0.dy) +
+			'L' + x0 + ',' + y0 +
+			'L' + x1 + ',' + y1 +
+			'L' + (x1 + c1.dx) + ',' + (y1 + c1.dy));
+		line.attr('stroke-width', WIRE_WIDTH);
+		line.attr('stroke-linecap', 'round');
+		c1.line = line;
+		my.DrawCirc.recolorConnection(info, c0);
+		return line;
+	};
+
+	my.DrawCirc.recolorConnection = function (info, conn) {
+		var color;
+
+		if (info.state && info.state.getValue(conn)) {
+			color = '#0A0';
+		} else {
+			color = '#030';
+		}
+		if (conn.stub) {
+			conn.stub.attr('stroke', color);
+		}
+		if (conn.circ) {
+			conn.circ.attr('stroke', color);
+			if (!conn.input) {
+				conn.circ.attr('fill', color);
+			}
+		}
+		$.each(conn.conns, function (i, c) {
+			if (c.stub) {
+				c.stub.attr('stroke', color);
+			}
+			if (c.circ) {
+				c.circ.attr('stroke', color);
+			}
+			if (c.line) {
+				c.line.attr('stroke', color);
+			}
+		});
+	};
+
+	my.DrawCirc.ghostWireToCoord = function (info, c0, x, y) {
+		var x0, y0, line;
+		x0 = c0.elt.x + c0.x;
+		y0 = c0.elt.y + c0.y;
+		line = info.paper.path('M' + (x0 + c0.dx) + ',' + (y0 + c0.dy) +
+			'L' + x0 + ',' + y0 +
+			'L' + x + ',' + y);
+		line.attr('opacity', 0.5);
+		line.attr('stroke-width', WIRE_WIDTH);
+		return line;
+	};
+
+	my.DrawCirc.ghostWireToConn = function (info, c0, c1) {
+		var x0, y0, x1, y1, line;
+		x0 = c0.elt.x + c0.x;
+		y0 = c0.elt.y + c0.y;
+		x1 = c1.elt.x + c1.x;
+		y1 = c1.elt.y + c1.y;
+		line = info.paper.path('M' + (x0 + c0.dx) + ',' + (y0 + c0.dy) +
+			'L' + x0 + ',' + y0 +
+			'L' + x1 + ',' + y1 +
+			'L' + (x1 + c1.dx) + ',' + (y1 + c1.dy));
+		line.attr('opacity', 0.5);
+		line.attr('stroke-width', WIRE_WIDTH);
+		return line;
+	};
+}(Circuit, jQuery || $));
