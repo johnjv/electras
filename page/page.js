9289e8b52bcb2ec3e5c0de95e91f0d28ea038498
Carl Burch <Carl Burch@localhost>
2012-05-25 23:30:12 -0400

    Improved page-turning effect, packaged page.js as a library

--- page/page.js
+++ page/page.js
@@ -1,44 +1,55 @@
-function transitionNext(src, dst) {
-	var dist;
-	src.css('z-index', 2);
-	dst.css('z-index', 1);
-	dst.css('top', 0);
-	dst.show();
-	dist = $('#clipboard').height();
-	src.height(dist - 100);
-	src.stop().animate({ top: -dist + 'px' }, 1000, function () {
-		src.hide();
-	});
-}
+var transitionCut, transitionNext, transitionPrev;
 
-function transitionPrev(src, dst) {
-	var dist;
-	src.css('z-index', 1);
-	dst.css('z-index', 2);
-	dist = $('#clipboard').height();
-	dst.height(dist - 100);
-	dst.css('top', -dist + 'px');
-	dst.show();
-	dst.stop().animate({ top: '0px' }, 1000, function () {
-		src.hide();
-	});
-}
+(function ($) {
+	"use strict";
+	
+	function positionPage(page) {
+		var vpad, hpad;
+		vpad = page.innerHeight() - page.height();
+		hpad = page.innerWidth() - page.width();
+		page.height(page.parent().height() - vpad);
+		page.width(page.parent().width() - hpad);
+		page.css('top', '-' + page.css('border-top-width'));
+		page.css('left', '-' + page.css('border-left-width'));
+		page.css('transform', 'scale(1)');
+	}
 
-$(document).ready(function () {
-	$('#clipboard').height($('body').height() - 200);
-	$('.page').hide();
-	$('#page1').show();
+	transitionCut = function (src, dst) {
+		positionPage(dst);
+		dst.show();
+		if (src) {
+			src.hide();
+		}
+	};
 
-	$('#page1').click(function (e) {
-		e.preventDefault();
-		transitionNext($('#page1'), $('#page2'));
-	});
-	$('#page2').click(function (e) {
-		e.preventDefault();
-		transitionNext($('#page2'), $('#page3'));
-	});
-	$('#page3').click(function (e) {
-		e.preventDefault();
-		transitionPrev($('#page3'), $('#page1'));
-	});
-});
+	transitionNext = function (src, dst) {
+		var d = Math.floor(dst.parent().height() / 2);
+		src.css('z-index', 2);
+		dst.css('z-index', 1);
+		positionPage(dst);
+		dst.show();
+		src.stop().animate({
+			top: '-=' + d + 'px',
+			transform: 'scale(1, 0.01)'
+		}, 1000, function () {
+			src.hide();
+			src.css('z-index', 0);
+		});
+	};
+
+	transitionPrev = function (src, dst) {
+		var d = Math.floor(dst.parent().height() / 2);
+		src.css('z-index', 1);
+		dst.css('z-index', 2);
+		positionPage(dst);
+		dst.css('top', '-=' + d + 'px');
+		dst.css('transform', 'scale(1, 0.01)');
+		dst.show();
+		dst.stop().animate({
+			top: '+=' + d + 'px',
+			transform: 'scale(1)'
+		}, 1000, function () {
+			src.hide();
+		});
+	};
+}(jQuery));
