89182547f3cd048f454e25899ce011d34333c04a
Brandon <Brandon@localhost>
2012-05-28 12:00:49 -0500

    Clipboard with a welcome page

--- levels/Clipboard.js
+++ levels/Clipboard.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	"use strict";	
-	$('#hintText').hide();
+	"use strict";
 	setUpLevel();
 	
 	$("#next").click(function(event){
@@ -16,7 +15,7 @@ $(document).ready(function(){
 	    var pagenumber = LevelSelector.getCurrentPage();	    	
         var page = $('#page' + pagenumber);        
         event.preventDefault();
-        if(num >= 0 && num <= 21){
+        if(num >= 0 && num <= 22){
             if(num > pagenumber){
                 transitionNext(page, $('#page' + num));
 	            $('#prev').fadeTo('slow', '1');
@@ -30,19 +29,19 @@ $(document).ready(function(){
                $('#hint').fadeTo('slow', '0.5');
                $('#showLevels').fadeTo('slow', '0.5');
             }
-            else if(num === 1){
+            else if(num === 2 || num === 1){
                $('#prev').fadeTo('slow', '1');
                $('#hint').fadeTo('slow', '0.5');
                $('#showLevels').fadeTo('slow', '0.5');
             }
-            else if(num === 21){
+            else if(num === 22){
                $('#next').fadeTo('slow', '0.5');            
             } 
             else{
                 $('#circuit').show();            
                 $('#hint').fadeTo('slow', '1');
                 $('#showLevels').fadeTo('slow', '1');            
-                LevelSelector.setLevel(all_levels[num-2]);
+                LevelSelector.setLevel(all_levels[num-3]);
             }		
             LevelSelector.setPage(num); 
             $('.hintText').hide();
@@ -53,14 +52,15 @@ $(document).ready(function(){
 	
 	$("#showLevels").click(function(){
 	    var level = LevelSelector.getCurrentLevel();
-	    if(LevelSelector.getCurrentPage() !== 0 && LevelSelector.getCurrentPage() !== 1){	
+	    var page = LevelSelector.getCurrentPage();
+	    if(page > 2){	
 	        if(level.levelid <= 10){
-	            goToPage(0);
-	            LevelSelector.setPage(0);	            
+	            goToPage(1);
+	            LevelSelector.setPage(1);	            
 	        }
 	        else{
-	            goToPage(1);
-	            LevelSelector.setPage(1);
+	            goToPage(2);
+	            LevelSelector.setPage(2);
 	        }
 	        if(level.complete){
 	            $('#row' + level.levelid).append('<td><img id = "check" src = "../levels/images/checkmark.png"></td>');
@@ -88,7 +88,7 @@ $(document).ready(function(){
 	});
 	
 	$("#hint").click(function(){
-	    if(LevelSelector.getCurrentPage() >= 2){
+	    if(LevelSelector.getCurrentPage() >= 3){
 	        if($(this).text() === "Show Hint"){		
 	            $('.hintText').show();
 	            $(this).text("Hide Hint");
@@ -98,111 +98,102 @@ $(document).ready(function(){
 	            $(this).text("Show Hint");
 	        }	
 	    }	
-	});      
-});	
+	}); 
 
-
-function getHint(){
-    var level = LevelSelector.getCurrentLevel();
-    return level.hint;
-}
-
-function transitionNext(src, dst) {
-	var dist;
-	src.css('z-index', 2);
-	dst.css('z-index', 1);
-	dst.css('top', 0);
-	dst.show();
-	dist = $('#clipOrder').height();
-	src.height(dist);
-	src.stop().animate({ top: -dist + 'px' }, 1000, function () {
-		src.hide();
-	});
-}
-
-function transitionPrev(src, dst) {
-	var dist;
-	src.css('z-index', 1);
-	dst.css('z-index', 2);
-	dist = $('#clipOrder').height();
-	dst.height(dist);
-	dst.css('top', -dist + 'px');
-	dst.show();
-	dst.stop().animate({ top: '0px' }, 1000, function () {
-		src.hide();
-	});
-} 
-
-function setUpLevel(){ 
-    Placer.place();     
-    $('#factory').hide();
-    $('#cliptip').hide();
-    makePages();
-    $('.page').hide();    
-    $('.hintText').hide();
-    $('#page0').show();
-    $('#hint').fadeTo('slow', '0.5');
-    $('#showLevels').fadeTo('slow', '0.5');
-    $('#prev').fadeTo('slow', '0.5');
-    $('#return').fadeTo('slow', '0.5'); 
-     
-}
-
-function makePages() {    
-    var i = 0;
-    for(var i = 0 ; i< 20; i += 1){
-        var level = all_levels[i]       
-        var page = $('<tr></tr>').addClass('rows').attr('id', 'row' + level.levelid);
-        var page2 = $('<tr></tr>').addClass('rows').attr('id', 'row' + level.levelid);
-        var order = $('<div></div>').addClass('page').attr('id', 'page' + (level.levelid + 1));     
-            
-        if(i <= 9){
-            page.append($('<td></td>').addClass('selectName').text(level.levelid + ". " + level.levelname)); 
-            $('#levels1').append(page);                              
-        }
-        else if(9 < i & i <=19){
-            page2.append($('<td></td>').addClass('selectName').text(level.levelid + ". " + level.levelname));
-            $('#levels2').append(page2);              
-        }
-        
-        order.append($('<div></div>').addClass('levelname').text(level.levelid + ". " + level.levelname));
-        order.append($('<div></div>').addClass('order').text("Order: " + level.orderText)); 
-        order.append($('<div></div>').addClass('hintText').text("Hint: " + level.hint));
-        $('#clipOrder').append(order); 
+    function getHint(){
+        var level = LevelSelector.getCurrentLevel();
+        return level.hint;
     }
-    addLevelClicks();
-}
 
-function addLevelClicks(){
-    $('#tbody1').children().each(function(i, child){
-        $(child).click(function(){
-            LevelSelector.setPage(i+2);
-            var pagenum = LevelSelector.getCurrentPage();  
-            LevelSelector.setLevel(all_levels[i]);          
-            transitionNext($('#page0'), $('#page' + pagenum ));            
-            $('#circuit').show();
-            $('#hint').fadeTo('slow', '1');
-            $('#showLevels').fadeTo('slow', '1');
-            $('#prev').fadeTo('slow', '1');
-            $('#return').fadeTo('slow', '1');
-//            $('#factory').show();
-            Placer.place();
-        });   
-    });
-    
-    $('#tbody2').children().each(function(i, child){
-        $(child).click(function(){
-            LevelSelector.setPage(i + 12);
-            var pagenum = LevelSelector.getCurrentPage();
-            transitionNext($('#page1'), $('#page' + pagenum));            
-            LevelSelector.setLevel(all_levels[i + 10]);
-            $('#circuit').show();
-            $('#hint').fadeTo('slow', '1');
-            $('#showLevels').fadeTo('slow', '1');
-            $('#prev').fadeTo('slow', '1');
-            $('#return').fadeTo('slow', '1');
-//            $('#factory').show();
-            Placer.place();
-        });   
-    });
-}
+    function transitionNext(src, dst) {
+	    var dist;
+	    src.css('z-index', 2);
+	    dst.css('z-index', 1);
+	    dst.css('top', 0);
+	    dst.show();
+	    dist = $('#clipOrder').height();
+	    src.height(dist);
+	    src.stop().animate({ top: -dist + 'px' }, 1000, function () {
+		    src.hide();
+	    });
+    }
+
+    function transitionPrev(src, dst) {
+	    var dist;
+	    src.css('z-index', 1);
+	    dst.css('z-index', 2);
+	    dist = $('#clipOrder').height();
+	    dst.height(dist);
+	    dst.css('top', -dist + 'px');
+	    dst.show();
+	    dst.stop().animate({ top: '0px' }, 1000, function () {
+		    src.hide();
+	    });
+    } 
+
+    function setUpLevel(){ 
+        Placer.place();     
+        $('#factory').hide();
+        $('#cliptip').hide();
+        makePages();
+        $('.page').hide();    
+        $('.hintText').hide();    
+        $('#page0').show();    
+        $('#hint').fadeTo('slow', '0.5');
+        $('#showLevels').fadeTo('slow', '0.5');
+        $('#prev').fadeTo('slow', '0.5');
+        $('#return').fadeTo('slow', '0.5');
+        $('#hintText').hide();     
+    }
+
+    function makePages() {    
+        var i = 0;
+        for(var i = 0 ; i< 20; i += 1){
+            var level = all_levels[i]       
+            var page = $('<tr></tr>').addClass('rows').attr('id', 'row' + level.levelid);
+            var page2 = $('<tr></tr>').addClass('rows').attr('id', 'row' + level.levelid);
+            var order = $('<div></div>').addClass('page').attr('id', 'page' + (level.levelid + 2));     
+                
+            if(i <= 9){
+                page.append($('<td></td>').addClass('selectName').text(level.levelid + ". " + level.levelname)); 
+                $('#levels1').append(page);                              
+            }
+            else if(9 < i & i <=19){
+                page2.append($('<td></td>').addClass('selectName').text(level.levelid + ". " + level.levelname));
+                $('#levels2').append(page2);              
+            }
+            
+            order.append($('<div></div>').addClass('levelname').text(level.levelid + ". " + level.levelname));
+            order.append($('<div></div>').addClass('order').text("Order: " + level.orderText)); 
+            order.append($('<div></div>').addClass('hintText').text("Hint: " + level.hint));
+            $('#clipOrder').append(order); 
+        }
+        addLevelClicks();    
+    }
+
+    function addLevelClicks(){
+        var page, pagenum;    
+        $('#tbody1').children().each(function(i, child){                                     
+            $(child).click(function(){ 
+                goToPage(i + 3);
+                $('#circuit').show();
+                $('#hint').fadeTo('slow', '1');
+                $('#showLevels').fadeTo('slow', '1');
+                $('#prev').fadeTo('slow', '1');
+                $('#return').fadeTo('slow', '1');            
+                Placer.place();
+            });   
+        });
+        $('#tbody2').children().each(function(i, child){                                     
+            $(child).click(function(){
+                goToPage(i + 13);
+                $('#circuit').show();
+                $('#hint').fadeTo('slow', '1');
+                $('#showLevels').fadeTo('slow', '1');
+                $('#prev').fadeTo('slow', '1');
+                $('#return').fadeTo('slow', '1');            
+                Placer.place();
+            });   
+        });
+    }
+});	
