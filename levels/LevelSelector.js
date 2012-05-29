9e79d6ceba6f3903d1e5183523eb85f15cb52385
Brandon <Brandon@localhost>
2012-05-29 16:47:33 -0500

    Clipboard with the new image, sort of have credits working

--- levels/LevelSelector.js
+++ levels/LevelSelector.js
@@ -8,33 +8,37 @@ var LevelSelector = (function($) {
         var temp;
         temp = level;
         level = change;
-        Circuit.levelChanged(temp, level);         
+        Circuit.levelChanged(temp, level);
+        FactoryFloor.levelChanged(temp, level);         
     }; 
-    
+
     my.setPage = function(change) {
         pagenumber = change;        
     }; 
 
-   my.getCurrentLevel = function(){     
-      return level;
+    my.getCurrentLevel = function(){     
+        return level;
     };
-    
-   my.getCurrentPage = function(){
-      return pagenumber;
+
+    my.getCurrentPage = function(){
+        return pagenumber;
+    };
+
+    my.setComplete = function(complete){
+        level.complete = complete;
     };
     
     my.windowResized = function () {
-        var par, self, container, clipPos;
-	    self = $('#clipboard');
-	    container = $('#main_container');
-	    clipPos = self.position();       
-        clipPos.left = container.width() * 0.4;
+        var par, self, container, clipPos, tip;
+        self = $('#clipboard');
+        container = $('#main_container');
+        tip = $('#cliptip');
+        clipPos = self.position();       
+        clipPos.left = (container.width() - self.width())/2        
         clipPos.top = container.height() * 0.15;
-        self.css('left', clipPos.left).css('top', clipPos.top);	    
-	    /*resizeClipboard();
-	    resizeCliporder();
-	    resizeClipbuttons();*/
-	};
+        self.css('left', clipPos.left).css('top', clipPos.top);	
+        tip.css('left', container.width() * 0.45).css('bottom' , '0%'); 
+    };
     
     return my;
 }(jQuery));
