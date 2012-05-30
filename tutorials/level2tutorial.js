317e4f9642132bf4eb154cd374d50d8fd3099094
jefferton <jefferton@ubuntu>
2012-05-30 16:54:17 -0500

    tutorial level 2 fixed

--- tutorials/level2tutorial.js
+++ tutorials/level2tutorial.js
@@ -1,15 +1,12 @@
 var its_a_start_events = function(){
-  //console.log("starting")
   var elements = new Elements(Circuit.getElements())
   var lightbulb_in = elements.lightbulb().first_connection()
   var empty_sensors = elements.sensor().empty().elements
-  //console.log('hello', elements)
   var first_sensor = elements.sensor().first_connection()
 
   if (empty_sensors && empty_sensors.length == 3) {
     var params = []
     $.each(empty_sensors, function(i, sensor){
-//console.log(sensor)
         var highlighted_thing = Filters.find_connection(sensor,'empty',false)
         var parametric_thing = parametric_to_square(highlighted_thing, true)
         params.push(parametric_thing)
@@ -18,7 +15,7 @@ var its_a_start_events = function(){
     createSpeechBubble(first_sensor, getBubble(2, 1));
   }
 
-  else if (elements.sensor('-').active_connection().exists() &&
+  else if (elements.sensor('\\|').active_connection().exists() &&
             elements.lightbulb().empty().exists()) {
     highlightSection(lightbulb_in, true);
     createSpeechBubble(first_sensor, getBubble(2, 2));
@@ -31,7 +28,7 @@ var its_a_start_events = function(){
   }
 
   else if (elements.lightbulb().input('filled').exists() &&
-          elements.sensor('-').empty().exists()) {
+          elements.sensor('\\|').empty().exists()) {
     Tutorial.unhighlightSection()
     createSpeechBubble(first_sensor, getBubble(2, 4));
   }
