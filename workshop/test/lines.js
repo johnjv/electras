737b5becc8bafd36d1114479a4d5f20c852ed922
Carl Burch <Carl Burch@localhost>
2012-05-30 13:12:52 -0500

    Clarified the lines.js example

--- workshop/test/lines.js
+++ workshop/test/lines.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-	var body, canv, paper, onDrag, onUp;
+	var canv, paper;
 
-	body = $('body');
 	canv = $('#canv');
 	paper = Raphael("canv", canv.width(), canv.height());
-	onDrag = null;
-	onUp = null;
 
 	function DrawLine(e) {
+		// An object is created each time the user presses the
+		// mouse or touches the screen, passing the event as a
+		// parameter. The event has pageX and pageY fields
+		// saying where the event occurred, in page coordinates.
 		e.preventDefault();
 		this.pos0 = 'M' + e.pageX + ',' + e.pageY;
 		this.path = paper.path(this.pos0);
 	};
 
 	DrawLine.prototype.onDrag = function (e) {
+		// The object's onDrag method is called each time the
+		// user moves the mouse (with the button pressed) on
+		// moves the finger. Again, the parameter event has pageX and pageY
+		// fields saying where the event occurred, in page coordinates.
 		var newpath;
 
 		e.preventDefault();
@@ -27,5 +32,20 @@ $(document).ready(function () {
 		this.path = newpath;
 	};
 
+	DrawLine.prototype.onRelease = function (e) {
+		// The object's onRelease method is called when the
+		// releases the mouse button or removes the finger from
+		// the screen.  Again, the parameter event has pageX and pageY
+		// fields saying where the event occurred, in page coordinates.
+		// It also has an "isTap" field that is true or false
+		// depending on whether the user has 'tapped' the screen
+		// (i.e., has not dragged very far from the initial location).
+
+		// In this example, we have nothing to do when the drag completes.
+	};
+
+	// Now we register the DrawLine class as the one to be
+	// instantiated each time the user starts to drag the mouse
+	// within the "canv" element on the screen.
 	multidrag.register(canv, DrawLine);
 });
